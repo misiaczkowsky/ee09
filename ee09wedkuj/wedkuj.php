8d25acc52930cd124f673f43a69d1be647ed5ab0
misiaczkowsky <72608680+misiaczkowsky@users.noreply.github.com>
2021-09-11 11:24:31 +0200

    Add files via upload

---
+++ ee09wedkuj/wedkuj.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Wędkujemy</title>
+    <link rel="stylesheet" href="styl_1.css">
+</head>
+<body>
+    <header>
+        <h1>Portal dla wędkarzy</h1>
+    </header>
+    <div id="lewy">
+        <h2>Ryby drapieżne naszych wód</h2>
+        <ul>
+        <?php
+          $db = new mysqli('localhost','root' ,'','wedkowanie');
+                               
+           $sql = "SELECT nazwa, wystepowanie FROM ryby where styl_zycia = 1";
+           $wynik = $db->query($sql);   
+           
+           while($wiersz = $wynik->fetch_assoc()) {
+               $nazwa = $wiersz ['nazwa'];
+               $wystepowanie = $wiersz ['wystepowanie'];
+               echo "<li>$nazwa, występowanie: $wystepowanie</li>";
+           }   
+           $db->close();           
+        ?>
+        </ul>
+    </div>
+    <div id="prawy">
+    
+        <img src="ryba1.jpg" alt="Sum"><br> 
+        <a href="kwerendy.txt">Pobierz kwerendy</a>
+    </div>
+    <footer>
+        <p>Stronę wykonał: michal </p>
+    </footer>
+
+
+</body>
+</html>
